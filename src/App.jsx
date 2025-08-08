11b757444023589f20785ad5cc2dfa339ec7a1db
Andebet3210 <andebetmolla1900@gmail.com>
2025-08-08 16:57:08 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/navbar/navbar.jsx';
+import Navbar from './components/Navbar/navbar.jsx';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Title from './components/Title/Title';
